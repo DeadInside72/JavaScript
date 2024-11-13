dd9a6839ba8586b2a0a76c6b43d08e306006566d
DeadInside72 <kostylev.ivan353@gmail.com>
2024-11-13 21:00:38 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+// //информация консоли 
+// console.log("hello,world.")
+// // Окно с сообщением
+// // alert ("метод alert")
+// // Диалоговое окно
+// nameUser = prompt("Как вас зовут?","нет данных")
+// alert(nameUser);
+
+
+
+
+
+// // окно с выбором
+// // confirm ("ок или отмена")
+// строка
+// let str1 = "str1";
+// let str2 ='str2';
+// let nameUser = "Иван";
+// // Первый способ - обратные кавычки
+// let info = `Привет,я ${nameUser}`;
+// // console.log(info);
+// // второй способ - конкатенация строк
+// let val1 = "Привет, я ";
+// let val2 = "Иван ";
+// let res = val1+val2; 
+// // console.log (res); 
+// // console.log(val1.length)
+// // console.log("Длина строки:" + val1 + " = " + val1.length)
+// let data = `Длина строки ${val1} = ${val1.length}`
+// console.log(data)
+
+// console.log(`Длина строки ${val1} = ${val1.length}`)
+
+// let str = "hello"
+// console.log (str.toUpperCase())
+
+// let res = str.toLowerCase();
+// console.log (res);
+
+// let info = "JavaScript"
+// console.log(info.includes("Java"));
+// console.log(info.includes("FJH"));
+
+// console.log(typeof 8);
+// console.log(typeof "str");
+// console.log(typeof true);
+
+// // Преобразование типов данных
+
+// let value = 11;
+// console.log(typeof String(value));
+
+// let value2 = "5";
+// console.log(typeof value2);
+// // 1 
+// let num = Number(value2)
+// console.log(typeof num)
+// // 2
+// let num2 = +value2
+// console.log(typeof num2)
+
+let str = "";
+console.log(Boolean(str));
+
+
+let str1 = "hello"
+console.log(Boolean(str1));
+
+let num = 0 
+console.log(Boolean(num));
+
+let num = 10
+console.log(Boolean(num));
